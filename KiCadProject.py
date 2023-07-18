00132d53d4658dce46a3cf10b8e16d3d9ad85dd3
Jessica Schneider <16026066+jschneider1207@users.noreply.github.com>
2023-07-17 22:35:30 -0500

    file classes, parse project

--- KiCadProject.py
+++ KiCadProject.py
@@ -1,12 +1,29 @@
+from __future__ import annotations
 from pathlib import Path
+from Excellon import Excellon
+from Gerber import Gerber
 
 
 class KiCadProject:
-    def __init__(self, dir: str) -> None:
-        self.dir = Path(dir)
-        if not self.dir.is_dir():
-            raise "Invalid directory path"
-        return
+    def __init__(self, project_dir: Path, output_dir: Path, gerber_files: list[Gerber], drill_files: list[Excellon]) -> None:
+        self.project_dir = project_dir
+        self.output_dir = output_dir
+        self.gerber_files = gerber_files
+        self.drill_files = drill_files
 
-    def output_files(self) -> list[str]:
-        return [p for p in self.dir.iterdir() if p.is_file()]
+    @classmethod
+    def load(dir: str) -> KiCadProject:
+        dir_path = Path(dir)
+        if not dir_path.is_dir():
+            raise 'Invalid directory path'
+
+        output_path = dir_path / 'out'
+        if not output_path.is_dir():
+            raise "Project output directory not found. Ensure the project contains a directory named 'out'."
+
+        gerber_files = [Gerber(f) for f in output_path.glob('**/*.gbr')]
+        drill_files = [Excellon(f) for f in output_path.glob('**/*.drl')]
+        if gerber_files.count() == 0:
+            raise 'No gerber files found in output directory. Did you forget to plot the board?'
+
+        return KiCadProject(dir_path, output_path, gerber_files, drill_files)
